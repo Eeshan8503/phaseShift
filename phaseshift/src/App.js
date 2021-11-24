89920c27b74613df28955a052f026c9887180a4f
Eeshan Mattey <71863350+Eeshan8503@users.noreply.github.com>
2021-11-24 11:19:47 +0530

    Update App.js

--- phaseshift/src/App.js
+++ phaseshift/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Qna from './components/QnA'
 function App() {
-  var i=1;
   return (
     <div className="App">
-     <Qna num={i++} question="what is your name" theme="1"/>
-     <Qna num={i++} question="what is your DOB" theme="2"/>
-     <Qna num={i++} question="what is your fav color" theme="3"/>
+     <Qna num={1} question="what is your name" theme="1"/>
+     <Qna num={2} question="what is your DOB" theme="2"/>
+     <Qna num={3} question="what is your fav color" theme="3"/>
     </div>
   );
 }
