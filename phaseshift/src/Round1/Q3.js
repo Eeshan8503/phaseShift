e0e2e9922262a00e0fb8f295d375045ddaeb9822
Eeshan8503 <Eeshan8503@users.noreply.github.com>
2021-11-25 12:32:28 +0530

    image added

--- phaseshift/src/Round1/Q3.js
+++ phaseshift/src/Round1/Q3.js
@@ -3,7 +3,7 @@ import QnA from '../components/QnA'
 const Q3 = () => {
     return (
         <div>
-     <QnA num={3} question="Decode the logo above and refer the decoded word to a physical thing which refers to a company whose parent organisation is responsible for a major digital disruption. Name the parent organisation." theme="1"  image="https://ibb.co/cwbY8MY"/>  
+     <QnA num={3} question="Decode the logo above and refer the decoded word to a physical thing which refers to a company whose parent organisation is responsible for a major digital disruption. Name the parent organisation." theme="1"  image="https://i.ibb.co/xYmFgPF/image.png"/>  
             
         </div>
     )
